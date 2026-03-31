38d008a90ec5cae505c129c0724c6f35cd6cce84
craigwat99 <craigwat99@gmail.com>
2026-03-31 14:27:56 +1300

    Update Navigation.tsx

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -52,8 +52,7 @@ export function Navigation({ onNavigate }: NavigationProps) {
               isScrolled ? 'text-[#784982]' : 'text-white'
             }`}
           >
-            <img src="/rainbow-heart.png" alt="" className="inline-block w-[1.4em] h-[1.4em] mr-1 align-middle relative -top-[0.05em]" />
-            <span className="font-black">40</span> YEARS
+            <img src="/40Y.svg" alt="" className="inline-block w-[1.4em] h-[1.4em] mr-1 align-middle relative -top-[0.05em]" />
           </a>
 
           {/* Desktop Navigation */}
